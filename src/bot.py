7e82dc8dbc14af236c1c585c7b3eb6dd7ba9cc79
Matus Toth <toth.matus@proton.me>
2026-04-18 16:38:50 +0200

    Init version telegram bot

--- src/bot.py
+++ src/bot.py
@@ -2,20 +2,48 @@ import requests
 from config import BOT_TOKEN
 from pymongo import MongoClient
 from datetime import datetime, timezone, timedelta
+from baml_client.types import Message, Debt
 
+BOT_USERNAME = "hack_kosice_bot"
 TELEGRAM_API = f"https://api.telegram.org/bot{BOT_TOKEN}"
+
+# class Debt(BaseModel):
+#     debtor: str
+#     creditor: str
+#     amount: float
+
+# class Message(BaseModel):
+#     user_name: str
+#     text: str
 
 client = MongoClient("mongodb://localhost:27017/")
 db = client["telegram_bot"]
 messages_col = db["messages"]
 
 def save_message(message):
-    messages_col.insert_one({
+    doc = {
         "chat_id": message["chat"]["id"],
         "user_id": message["from"]["id"],
-        "text": message.get("text", ""),
-        "timestamp": datetime.now(timezone.utc)
-    })
+        "username": message["from"].get("username"),
+        "timestamp": datetime.now(timezone.utc),
+    }
+
+    # text message
+    if "text" in message:
+        doc["type"] = "text"
+        doc["text"] = message["text"]
+
+    # photo message
+    elif "photo" in message:
+        doc["type"] = "photo"
+
+        # Telegram sends multiple sizes → last one is highest quality
+        photo = message["photo"][-1]
+
+        doc["file_id"] = photo["file_id"]
+        doc["caption"] = message.get("caption", "")
+
+    messages_col.insert_one(doc)
 
 def send_message(chat_id, text):
     url = f"{TELEGRAM_API}/sendMessage"
@@ -36,17 +64,25 @@ def get_last_hour_messages(chat_id):
 
     return list(cursor)
 
+def load_photo_message(msg):
+    pass
+
+def load_text_message(msg):
+    return Message(msg.get("username"), msg.get("text"))
+
 def handle_message(message):
     save_message(message)
 
     chat_id = message["chat"]["id"]
     text = message.get("text", "")
-    text_lower = text.lower()
 
-    # TODO Agent stuff
-    if text_lower.contains() == "hi":
-        send_message(chat_id, "👋 Hello!")
-    elif text_lower == "/help":
-        send_message(chat_id, "Commands: hi, /help")
-    else:
-        send_message(chat_id, f"You said: {text}")
+    if f"@{BOT_USERNAME.lower()}" not in text.lower():
+        return
+
+    messages = get_last_hour_messages(chat_id)
+    output = []
+    for msg in messages:
+        if msg.get("type") == "photo":
+            pass # TODO
+        else:
+            output.append(load_text_message(msg))
